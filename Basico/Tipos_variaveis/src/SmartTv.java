3486eb26f8c12453353815a9e072e2f7d8deb96b
Mateus Cordeiro <167920848+Cordeiro010@users.noreply.github.com>
2024-05-12 22:38:31 -0300

    add project Smart TV

---
+++ Basico/Tipos_variaveis/src/SmartTv.java
@@ -0,0 +1,34 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal(int novoCanaL){
+        canal = novoCanaL;
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.err.println("Aumentando volume para:" + volume);
+    }
+    
+    public void diminuirVolume(){
+        volume--;
+        System.err.println("Diminuindo volume para:" + volume);
+    }
+
+}
